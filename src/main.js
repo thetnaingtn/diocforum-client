c416a2bda571463b30f7b2f14bb1711c57d42d2c
Thet Naing Tun <thetnaingtun@bindez.com>
2021-07-13 22:40:51 +0630

    set up vue-router and element-plus ui

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
+import ElementPlus from "element-plus"
+import router from "./router";
+import "element-plus/lib/theme-chalk/index.css"
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
